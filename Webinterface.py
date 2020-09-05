cf0a33700390fc6516c8a5d61ac6294c9aa2de2a
Laborratte5 <rirussi.rr@gmail.com>
2020-09-05 16:25:56 +0200

    Added API Error Messages electric-meter add
    - Added error messages for missing parameter, invalid parameter type or parameter value
    - Automatically start development server if flask is in development mode

--- Webinterface.py
+++ Webinterface.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, request, abort, make_response, jsonify
 
 app = Flask(__name__)
 
+# TODO dokumentieren und verwenden
+api_codes = {"missing_parameter": 100,
+             "invalid_type": 200,
+             "invalid_values": 300
+             }
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -38,7 +44,33 @@ def electric_meter():
 
 @app.route('/api/json/electric-meter/add')
 def add_electric_meter():
-    return 'add electric meter'
+    # TODO write test for parse_parameter_json
+    params = parse_parameter_json((('value', float), ('pin', int), ('active-low', bool), ('name', str)))
+
+    invalid_parameter_value = []
+    # Check parameter value
+    # TODO objekt das in liste eingefügt wird vielleicht über funktion auslagern
+    # TODO vllt auch noch api status code einfügen
+    if params['value'] <= 0:
+        invalid_parameter_value.append({"parameter": "value",
+                                        "message": "must be greater than 0"
+                                        })
+    if params['pin'] <= 0:
+        invalid_parameter_value.append({"parameter": "pin",
+                                        "message": "must be greater than 0"
+                                        })
+    # TODO Namenslänge ohne whitespace character > 0
+    if not bool(params['name']):
+        invalid_parameter_value.append({"parameter": "name",
+                                        "message": "must contain text"
+                                        })
+
+    if len(invalid_parameter_value) > 0:
+        abort_parameter('invalid parameter value', invalid_parameter_value)
+
+
+
+    return ''
 
 @app.route('/api/json/electric-meter/remove')
 def remove_electric_meter():
@@ -61,4 +93,68 @@ def add_database():
 def remove_database():
     return 'remove database'
 
-#app.run()
+
+# Helper functions
+# TODO test
+def parse_parameter_json(expected_parameter, arguments=None):
+    if arguments is None:
+        arguments = request.args
+
+    # Check if parameter is of type 'param_type'
+    def is_param_of_type(param_name, param_type):
+        param = arguments[param_name]
+        # Test bool
+        if param_type is bool:
+            return param in ('True', 'true', 'False', 'false')
+        else:
+            # Test other types by trying to cast param
+            try:
+                param_type(param)
+                return True
+            except ValueError:
+                return False
+
+    # Parse param into specified param_type
+    def parse_param(param_name, param_type):
+        param = arguments[param_name]
+        # Special case for parsing bool
+        if param_type is bool:
+            return param in ('True', 'true')
+        return param_type(param)
+
+    # Create list of parameters missing in arguments
+    missing_parameters = [param for param, param_type in expected_parameter if param not in arguments.keys()]
+    # return json error
+    if len(missing_parameters) > 0:
+        abort_parameter('missing parameters', missing_parameters)
+        """json_message = jsonify({
+            "code": 400,
+            "info": "missing parameters",
+            "parameters": missing_parameters
+        })
+        response = make_response(json_message, 400)
+        abort(response)"""
+
+    # Create list of parameters having the wrong type
+    invalid_types = [{"parameter": param, "expected_type": str(param_type)}
+                     for param, param_type in expected_parameter
+                     if not is_param_of_type(param, param_type)]
+
+    if len(invalid_types) > 0:
+        abort_parameter('invalid parameter type', invalid_types)
+
+    dic = {param_name: parse_param(param_name, param_type) for param_name, param_type in expected_parameter}
+    return dic
+
+
+def abort_parameter(info, parameter_list):
+    json_message = jsonify({
+        "code": 400,
+        "info": info,
+        "parameters": parameter_list
+    })
+    response = make_response(json_message, 400)
+    abort(response)
+
+if app.config['ENV'] == 'development':
+    app.run()
--- unit-test/WebApiAccessTest.py
+++ unit-test/WebApiAccessTest.py
@@ -1,58 +1,61 @@
 import unittest
 
-
 from unittest import TestCase
 import requests
 
 
 class WebApiAccessTest(TestCase):
 
+    def setUp(self):
+        self.baseURL = 'http://33.33.33.33/'
+        self.jsonApiURL = 'api/json/'
+
     def test_api_data(self):
-        response = requests.get('http://127.0.0.1:5000/api/json/data')
+        response = requests.get(self.baseURL + self.jsonApiURL + 'data')
         self.assertNotEqual(response.status_code, 404)
 
     def test_api_data_day(self):
-        response = requests.get('http://127.0.0.1:5000/api/json/data/day')
+        response = requests.get(self.baseURL + self.jsonApiURL + 'data/day')
         self.assertNotEqual(response.status_code, 404)
 
     def test_api_data_week(self):
-        response = requests.get('http://127.0.0.1:5000/api/json/data/week')
+        response = requests.get(self.baseURL + self.jsonApiURL + 'data/week')
         self.assertNotEqual(response.status_code, 404)
 
     def test_api_data_month(self):
-        response = requests.get('http://127.0.0.1:5000/api/json/data/month')
+        response = requests.get(self.baseURL + self.jsonApiURL + 'data/month')
         self.assertNotEqual(response.status_code, 404)
 
     def test_api_data_year(self):
-        response = requests.get('http://127.0.0.1:5000/api/json/data/year')
+        response = requests.get(self.baseURL + self.jsonApiURL + 'data/year')
         self.assertNotEqual(response.status_code, 404)
 
     def test_api_electric_meter(self):
-        response = requests.get('http://127.0.0.1:5000/api/json/electric-meter')
+        response = requests.get(self.baseURL + self.jsonApiURL + 'electric-meter')
         self.assertNotEqual(response.status_code, 404)
 
     def test_api_electric_meter_add(self):
-        response = requests.get('http://127.0.0.1:5000/api/json/electric-meter/add')
+        response = requests.get(self.baseURL + self.jsonApiURL + 'electric-meter/add')
         self.assertNotEqual(response.status_code, 404)
 
     def test_api_electric_meter_remove(self):
-        response = requests.get('http://127.0.0.1:5000/api/json/electric-meter/remove')
+        response = requests.get(self.baseURL + self.jsonApiURL + 'electric-meter/remove')
         self.assertNotEqual(response.status_code, 404)
 
     def test_api_electric_meter_change(self):
-        response = requests.get('http://127.0.0.1:5000/api/json/electric-meter/change')
+        response = requests.get(self.baseURL + self.jsonApiURL + 'electric-meter/change')
         self.assertNotEqual(response.status_code, 404)
 
     def test_api_database(self):
-        response = requests.get('http://127.0.0.1:5000/api/json/database')
+        response = requests.get(self.baseURL + self.jsonApiURL + 'database')
         self.assertNotEqual(response.status_code, 404)
 
     def test_api_database_add(self):
-        response = requests.get('http://127.0.0.1:5000/api/json/database/add')
+        response = requests.get(self.baseURL + self.jsonApiURL + 'database/add')
         self.assertNotEqual(response.status_code, 404)
 
     def test_api_database_remove(self):
-        response = requests.get('http://127.0.0.1:5000/api/json/database/remove')
+        response = requests.get(self.baseURL + self.jsonApiURL + 'database/remove')
         self.assertNotEqual(response.status_code, 404)
 
 
