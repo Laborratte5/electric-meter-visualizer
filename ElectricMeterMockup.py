5872ad1677d21acd76b5a3abf6c5d448d41bfde3
Laborratte5 <rirussi.rr@gmail.com>
2020-10-03 15:37:32 +0200

    Setting count on random value on reset

--- ElectricMeterMockup.py
+++ ElectricMeterMockup.py
@@ -1,21 +1,20 @@
+import random
+
 
 class ElectricMeterMockup:
 
     def __init__(self, value, pin, active_low, name):
-        print('new electric meter with: ', value, pin, active_low, name)
         self.value = value
         self.pin = pin
         self.active_low = active_low
         self.name = name
         self.count = 0
-        # TODO pin interrupts
-        pass
 
     def get_amount(self):
         return self.count * self.value
 
     def reset(self):
-        self.count = 0
+        self.count = random.randint(0, 100)
 
     def set_count(self, new_count):
         self.count = new_count
