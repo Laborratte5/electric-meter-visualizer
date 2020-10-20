6bc31d8d309fba0d420ce7fc7d92a30ff3057e7f
Laborratte5 <rirussi.rr@gmail.com>
2020-10-20 15:26:02 +0200

    Added ElectricMeter attributes

--- ElectricMeter.py
+++ ElectricMeter.py
@@ -3,6 +3,10 @@ class ElectricMeter():
     def __init__(self, value, pin, active_low, name):
         self.value = value
         self.count = 0
+        self.name = name
+        self.pin = pin
+        self.active_low = active_low
+
         # TODO pin interrupts
         pass
 
