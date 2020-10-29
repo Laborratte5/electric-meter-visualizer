8963ecff2dce8bbdea85773f1712ac61fb592da3
Laborratte5 <rirussi.rr@gmail.com>
2020-10-29 16:36:31 +0100

    Changed LogicTest to fit new database system
    - Started to convert DatabaseMockup to new database system
    - Improved test_add_electric_meter(), test_remove_electric_meter()
    - Improved test_change_electric_meter()
    - Removed old test_read_electric_meters() test (TODO)
    - Added test methods for test_get_XY() (TODO)

--- unit-test/LogicTest.py
+++ unit-test/LogicTest.py
@@ -1,33 +1,39 @@
+import string
 import unittest
+import random
 
 from ElectricMeterMockup import ElectricMeterMockup
-from Database import Datasource as DS
 from Logic import Logic
 
 
+# TODO überarbeiten (Logik mit neuer Datenbank)
 class DatabaseMock:
 
     def __init__(self):
-        self.data_sources = []
-        self.data = {}
-
-    def add_data(self, data_list, time='N'):
-        for ds, value in zip(self.data_sources, data_list):
-            self.data[ds] = value
-
-    def get_data(self, start_time=None, end_time='now'):
-        return self.data
-
-    def add_data_source(self, ds):
-        self.data_sources.append(ds)
-
-    def remove_data_source(self, ds):
-        self.data_sources.remove(ds)
-
-    def add_rrd_archive(self, rra):
         pass
 
-    def remove_rrd_archive(self, idx):
+    def add_data_src(self, data_src_name):
+        pass
+
+    def remove_data_src(self, data_src_name):
+        pass
+
+    def add_data(self, data, data_src):
+        pass
+
+    def get_raw(self, delta=0):
+        pass
+
+    def get_day(self, delta=0):
+        pass
+
+    def get_month(self, delta=0):
+        pass
+
+    def get_year(self, delta=0):
+        pass
+
+    def get_years(self, delta=0):
         pass
 
 
@@ -36,130 +42,99 @@ class LogicTest(unittest.TestCase):
     def setUp(self):
         self.logic = Logic(True)
         self.db_mock = DatabaseMock()
-        self.db_mock.add_data_source(DS('dummy_datasource', 'GAUGE', 0))
         self.logic.database = self.db_mock
 
     def test_add_electric_meter(self):
-        data_src_prev_len = len(self.db_mock.data_sources)
-        electric_meters_prev_len = len(self.logic.electric_meters)
-        self.logic.add_electric_meter(0, 0, False, 'Meter1')
+        pre_add_len = len(self.logic.electric_meters)
 
-        # Check if Datasource was created
-        self.assertGreater(len(self.db_mock.data_sources), 1) # > 1 because of dummy_ds in Database
-        self.assertGreater(len(self.logic.datasources), 0)
-        # Check if Electric meter was created
-        self.assertGreater(len(self.logic.datasource_electric_meter_mapping.keys()), 0)
-        self.assertGreater(len(self.logic.electric_meters), 0)
+        # Create random values
+        value = random.random() * random.randint(1, 100)
+        pin = random.randint(1, 10)
+        active_low = random.choice([True, False])
+        name = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+
+        # Add electric meter
+        new_meter, id = self.logic.add_electric_meter(value, pin, active_low, name)
+
+        # Assertion
+        self.assertGreater(len(self.logic.electric_meters), pre_add_len)
+
+        self.assertEqual(value, new_meter.value)
+        self.assertEqual(pin, new_meter.pin)
+        self.assertEqual(active_low, new_meter.active_low)
+        self.assertEqual(name, new_meter.name)
+
+        self.assertEqual(new_meter, self.logic.get_electric_meter(id))
 
     def test_remove_electric_meter(self):
-        # Setup
-        # Create ElectricMeter
-        em1 = ElectricMeterMockup(10, 0, False, 'em1')
-        em2 = ElectricMeterMockup(20, 0, False, 'em2')
-        # Associate ElectricMeter with id
-        self.logic.electric_meters[1] = em1
-        self.logic.electric_meters[2] = em2
-        # Create Datasource
-        ds1 = DS('em1', 'GAUGE', 1800)
-        ds2 = DS('em2', 'GAUGE', 1800)
-        self.db_mock.add_data_source(ds1)
-        self.db_mock.add_data_source(ds2)
-        # Associate Datasource with ElectricMeter
-        self.logic.datasource_electric_meter_mapping[ds1] = em1
-        self.logic.datasource_electric_meter_mapping[ds2] = em2
-        # Add Datasource to DatasourceList
-        self.logic.datasources.append(ds1)
-        self.logic.datasources.append(ds2)
+        # Add electric meter
+        electric_meter_id = 0
+        electric_meter = ElectricMeterMockup(1, 1, False, 'electric meter to be removed')
+        self.logic.electric_meters[electric_meter_id] = electric_meter
+        self.logic._next_id = electric_meter_id + 1
 
-        # Remove Electric Meter 2
-        self.logic.remove_electric_meter(2)
+        pre_remove_len = len(self.logic.electric_meters)
+        # Remove electric meter
+        removed_meter = self.logic.remove_electric_meter(electric_meter_id)
 
-        # Check if Electric Meter 2 was removed
-        self.assertNotIn(em2, self.logic.electric_meters.values())
-        self.assertNotIn(ds2, self.db_mock.data_sources)
-        self.assertNotIn(ds2, self.logic.datasources)
-        self.assertNotIn(em2, self.logic.datasource_electric_meter_mapping.keys())
-        self.assertNotIn(ds2, self.logic.datasource_electric_meter_mapping.values())
-        self.assertIn(em1, self.logic.electric_meters.values())
-        self.assertIn(ds1, self.db_mock.data_sources)
-        self.assertIn(ds1, self.logic.datasources)
-        self.assertIn(ds1, self.logic.datasource_electric_meter_mapping.keys())
-        self.assertIn(em1, self.logic.datasource_electric_meter_mapping.values())
-
-    def test_remove_last_electric_meter(self):
-        # Setup
-        # Create ElectricMeter
-        em1 = ElectricMeterMockup(10, 0, False, 'em1')
-        # Associate ElectricMeter with id
-        self.logic.electric_meters[1] = em1
-        # Create Datasource
-        ds1 = DS('em1', 'GAUGE', 1800)
-        self.db_mock.add_data_source(ds1)
-        # Associate Datasource with ElectricMeter
-        self.logic.datasource_electric_meter_mapping[ds1] = em1
-        # Add Datasource to DatasourceList
-        self.logic.datasources.append(ds1)
-
-        # Remove Electric Meter 1 but dummy datasource should still exists
-        self.logic.remove_electric_meter(1)
-
-        # Check if Electric Meter 1 was removed
-        self.assertNotIn(em1, self.logic.electric_meters.values())
-        self.assertEqual(len(self.db_mock.data_sources), 1)  # Dummy Datasource
-        self.assertNotIn(ds1, self.db_mock.data_sources)
-        self.assertNotIn(em1, self.logic.datasource_electric_meter_mapping.keys())
-        self.assertNotIn(ds1, self.logic.datasource_electric_meter_mapping.values())
-        self.assertNotIn(ds1, self.logic.datasources)
-
-    def test_get_electric_meters(self):
-        # TODO write test
-        # Test really necessary?
-        pass
+        # Check if electric meter has been removed
+        self.assertGreater(pre_remove_len, len(self.logic.electric_meters))
+        self.assertNotIn(electric_meter, self.logic.electric_meters.values())
+        # Check if correct electric meter has been removed
+        self.assertEqual(removed_meter, electric_meter)
 
     def test_change_electric_meter(self):
         # Setup
-        self.logic.add_electric_meter(10, 0, False, 'em1')
+        electric_meter = ElectricMeterMockup(10, 0, False, 'em1')
+        electric_meter_id = 10
+        self.logic.electric_meters[electric_meter_id] = electric_meter
+        self.logic._next_id = electric_meter_id + 1
 
         # Change Electric Meter
-        self.logic.change_electric_meter(self.logic.next_id, 100, 500, True, 'changed_em1')
+        self.logic.change_electric_meter(electric_meter_id, 100, 500, True, 'changed_em1')
 
         # Check if Electric Meter data was changed
-        em = self.logic.get_electric_meter(self.logic.next_id)
+        em = self.logic.get_electric_meter(electric_meter_id)
         self.assertEqual(em.value, 100)
         self.assertEqual(em.pin, 500)
         self.assertEqual(em.active_low, True)
         self.assertEqual(em.name, 'changed_em1')
 
     def test_read_electric_meters(self):
-        # Setup
-        em1 = ElectricMeterMockup(1, 0, False, 'Electric Meter #1')
-        em2 = ElectricMeterMockup(1, 0, False, 'Electric Meter #2')
-        self.logic.electric_meters[0] = em1
-        self.logic.electric_meters[1] = em2
-        ds1 = DS('ds1', 'GAUGE', 2)
-        ds2 = DS('ds2', 'GAUGE', 2)
-        self.logic.datasources.append(ds1)
-        self.logic.datasources.append(ds2)
-        self.logic.datasource_electric_meter_mapping[ds1] = em1
-        self.logic.datasource_electric_meter_mapping[ds2] = em2
-        self.db_mock.add_data_source(ds1)
-        self.db_mock.add_data_source(ds2)
+        # TODO
+        # _read_electric_meters fragt alle electric meter ab und speichert deren Werte in die Datenbank
+        self.assertTrue(False)
+        pass
 
-        data1 = 10
-        data2 = 8
+    def test_get_raw(self):
+        # TODO
+        self.assertTrue(False)
+        pass
 
-        # Test
-        em1.set_count(data1)
-        em2.set_count(data2)
-        self.logic._read_electric_meters()
+    def test_get_day(self):
+        # TODO
+        self.assertTrue(False)
+        pass
 
-        # Test if data was added to database
-        data = self.db_mock.get_data()
+    def test_get_week(self):
+        # TODO
+        self.assertTrue(False)
+        pass
 
-        self.assertIn(data1, data.values())
-        self.assertIn(data2, data.values())
-        self.assertEqual(em1.get_amount(), 0)
-        self.assertEqual(em2.get_amount(), 0)
+    def test_get_month(self):
+        # TODO
+        self.assertTrue(False)
+        pass
+
+    def test_get_year(self):
+        # TODO
+        self.assertTrue(False)
+        pass
+
+    def test_get_years(self):
+        # TODO
+        self.assertTrue(False)
+        pass
 
 
 if __name__ == '__main__':
