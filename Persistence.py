7917043839014fe7c508b667dd3e703f714fe91e
Laborratte5 <rirussi.rr@gmail.com>
2020-12-25 19:08:42 +0100

    Implemented save_state
    - Implemented attribute getter
    - Added StateJsonEncoder

--- Persistence.py
+++ Persistence.py
@@ -1,3 +1,4 @@
+import json
 
 STATE_FILE = 'state.json'
 
@@ -9,19 +10,54 @@ class State:
         pass
 
     def save_state(self):
-        pass
+        with open(STATE_FILE, 'w') as f:
+            json.dump(self, f, cls=StateJsonEncoder)
+
+    def __init__(self):
+        # Initialize with default values
+        self.next_id = 10
+        self.electric_meters = {}
 
     def get_next_id(self):
-        pass
+        return self.next_id
 
     def set_next_id(self, next_id):
-        pass
+        self.next_id = next_id
+        self.save_state()
 
     def get_electric_meters(self):
-        pass
+        return self.electric_meters
 
     def set_electric_meters(self, electric_meters):
-        pass
+        self.electric_meters = electric_meters
+        self.save_state()
+
+
+class StateJsonEncoder(json.JSONEncoder):
+    def encode_state(self, state):
+        return {
+            'next_id': state.get_next_id(),
+            'electric_meters': [{
+                    'id': meter_id,
+                    'name': meter.name,
+                    'value': meter.value,
+                    'pin': meter.pin,
+                    'active_low': meter.active_low,
+                    'count': meter.count
+                } for meter_id, meter in state.electric_meters.items()]
+        }
+
+    def default(self, o):
+        if isinstance(o, State):
+            # State to dict
+            return self.encode_state(o)
+        else:
+            return json.JSONEncoder.default(self, o)
+
+
+class StateJsonDecoder:
+    # TODO
+    pass
 
 
 class InvalidStateFileException(Exception):
