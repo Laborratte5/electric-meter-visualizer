dfbe0be3df17c41bb40ae74c40f1984b84ae82c6
Laborratte5 <rirussi.rr@gmail.com>
2021-06-07 15:15:36 +0200

    test: Changed TestPlugin to fit PluginSPI

--- unit-test/testplugins/TestPlugin.py
+++ unit-test/testplugins/TestPlugin.py
@@ -1,9 +1,24 @@
 import logging
 
-from Plugin import PluginSPI
+from Plugin import ElectricMeterPluginSPI
 
 
-class Plugin(PluginSPI):
+class Plugin(ElectricMeterPluginSPI):
+
+    def create_electric_meter(self, name, custom):
+        pass
+
+    def json_serialize_electric_meter(self, electric_meter):
+        pass
+
+    def json_load_electric_meter(self, serialized_json):
+        pass
+
+    def change_electric_meter(self, electric_meter, name, custom):
+        pass
+
+    def validate_custom_data(self, custom):
+        pass
 
     def __init__(self, logger=logging.getLogger()):
         super().__init__(logger)
