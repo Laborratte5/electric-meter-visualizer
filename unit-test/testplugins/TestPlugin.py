af922c80bb362cfe81cfe05c9252a0010c298e75
Laborratte5 <rirussi.rr@gmail.com>
2021-06-07 15:10:53 +0200

    test: Added logger to TestPlugin

--- unit-test/testplugins/TestPlugin.py
+++ unit-test/testplugins/TestPlugin.py
@@ -1,6 +1,10 @@
+import logging
+
+from Plugin import PluginSPI
 
 
-class Plugin:
+class Plugin(PluginSPI):
 
-    def __init__(self):
-        print('TEST PLUGIN')
+    def __init__(self, logger=logging.getLogger()):
+        super().__init__(logger)
+        logger.info('TEST PLUGIN')
