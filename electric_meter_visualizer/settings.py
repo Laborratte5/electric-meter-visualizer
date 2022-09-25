ad91aa5a6ad65808919ea49c350eec12601ab577
Laborratte5 <rirussi.rr@gmail.com>
2022-09-25 14:28:50 +0200

    fix: Type hint for ALLOWED_HOSTS

--- electric_meter_visualizer/settings.py
+++ electric_meter_visualizer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-4r+&+y&p9+8lb=%feof&-lbh8qm(ka$229jl_h#=e-(%2mt_9-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
