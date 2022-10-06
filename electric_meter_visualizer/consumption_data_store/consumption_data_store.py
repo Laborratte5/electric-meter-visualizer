6cac13e1591d9f29fc754a7eb8b4566a9edefec5
Laborratte5 <rirussi.rr@gmail.com>
2022-10-06 21:46:32 +0200

    feat(ConsumptionDataStore): Add QueryBuilder

--- electric_meter_visualizer/consumption_data_store/consumption_data_store.py
+++ electric_meter_visualizer/consumption_data_store/consumption_data_store.py
@@ -48,3 +48,71 @@ class Query(abc.ABC):
     @abc.abstractmethod
     def __hash__(self):
         raise NotImplementedError
+
+
+class QueryBuilder(abc.ABC):
+    """
+    Used to create concrete Query objects
+    """
+
+    @abc.abstractmethod
+    def filter_bucket(self, bucket_list: set[str]) -> "QueryBuilder":
+        """
+        Filter the consumption data based on the buckets this data resides
+
+        Arguments:
+            - bucket_list must be a set of string
+              Only datapoints from a bucket contained in this list will be returned
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def filter_source(self, id_list: set[int]) -> "QueryBuilder":
+        """
+        Filter the consumption data based on the source id
+
+        Arguments:
+            - id_list must be a set of int
+              Only datapoints from sources contained in this list will be returned
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def filter_start(self, start_date: datetime) -> "QueryBuilder":
+        """
+        Filter the consumption data based on the start_date of the measurement
+
+        Arguments:
+            - start_date must be datetime
+              Only datapoints with a timestamp at start_date or later will be returned
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def filter_stop(self, stop_date: datetime) -> "QueryBuilder":
+        """
+        Filter the consumption data basaed on the stop_date of the measurement
+
+        Arguments:
+            - stop_date must be datetime
+              Only datapoints with a timestamp earlier than stop_date will be returned
+        """
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def build(
+        self, aggregate_window: timedelta, aggregate_functions: set[AggregateFunction]
+    ) -> Query:
+        """
+        Create the Query specified with this builder object
+
+        Arguments:
+            - aggregate_window must be timedelta
+              Datapoints are grouped into this aggregate_window based on their timestamp
+            - aggregate_functions must be a set of AggregateFunctions
+              All Datapoints inside an aggregate_window will be aggregated using the functions
+              in aggregate_functions.
+              The result will contain one datapoint for each aggregate_window.
+              The resulting datapoints contain one value for each aggregate_function.
+        """
+        raise NotImplementedError
